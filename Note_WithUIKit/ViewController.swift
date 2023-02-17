08f8e14e052f727f648958a6469bc8ef64156ae7
Tedtya Rady <tedtyarady@gmail.com>
2023-02-17 22:48:41 +1100

    Initial Commit

---
+++ Note_WithUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Note_WithUIKit
+//
+//  Created by Tedtya rady on 17/2/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
