3486a095cec8d84e17feaea8f956fff7c37ef7ff
Tedtya Rady <tedtyarady@gmail.com>
2023-02-18 16:05:12 +1100

    [Action]: - Added table view cell for audio note - update table view cell for reminder note

---
+++ Note_WithUIKit/AudioNote_TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AudioNote_TableViewCell.swift
+//  Note_WithUIKit
+//
+//  Created by Tedtya rady on 18/2/2023.
+//
+
+import UIKit
+
+class AudioNote_TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
