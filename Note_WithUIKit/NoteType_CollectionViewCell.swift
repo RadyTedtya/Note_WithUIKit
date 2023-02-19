67adbbc038408961dbad4ba71cf1da8fd472dba5
Tedtya Rady <tedtyarady@gmail.com>
2023-02-19 15:43:07 +1100

    [Action]: - added collection view cell - design collection view cell in xib

---
+++ Note_WithUIKit/NoteType_CollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  NoteType_CollectionViewCell.swift
+//  Note_WithUIKit
+//
+//  Created by Tedtya rady on 19/2/2023.
+//
+
+import UIKit
+
+class NoteType_CollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
