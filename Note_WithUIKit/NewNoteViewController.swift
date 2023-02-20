10ab81baf9e822e54d77a8d93718d676e2a46f86
Tedtya Rady <tedtyarady@gmail.com>
2023-02-20 19:32:30 +1100

    [Added]: - NewNoteViewController.swift - NewNoteViewController.xib

---
+++ Note_WithUIKit/NewNoteViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NewNoteViewController.swift
+//  Note_WithUIKit
+//
+//  Created by Tedtya rady on 20/2/2023.
+//
+
+import UIKit
+
+class NewNoteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
