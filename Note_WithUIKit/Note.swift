91dae71f00bd5e4df0d9eeb4d787d7f19a5573a2
Tedtya Rady <tedtyarady@gmail.com>
2023-02-20 19:35:50 +1100

    [Added] & [Modified] - Note.swift
    [Action]
    - Added and modified note class

--- Note_WithUIKit/Note.swift
+++ Note_WithUIKit/Note.swift
@@ -7,6 +7,16 @@
 
 import Foundation
 
+enum NoteType: String, CaseIterable, Identifiable {
+    var id: Self {
+        return self
+    }
+    case allNotes = "All Notes"
+    case audioNote = "Audio"
+    case reminderNote = "Reminder"
+    case imageNote = "Image"
+}
+
 struct Note: Identifiable {
     var id: Int
     var title: String!
@@ -18,7 +28,7 @@ struct Note: Identifiable {
     
     static let description = "Tum dicere exorsus est laborum et via procedat oratio quaerimus igitur, quid sit. Si sine causa? quae fuerit causa, mox videro; interea hoc tenebo, si ob……"
     
-    static let dummyImageNote: Note = .init(id: 1, title: "summer vacation", date: "12 Jan 2021", noteType: .imageNote, audio: nil, image: "image", description: description)
+    static let dummyImageNote: Note = .init(id: 1, title: "summer vacation", date: "12 Jan 2021", noteType: .imageNote, audio: nil, image: "image", description: "Tum dicere exorsus est laborum et via procedat oratio quaerimus igitur, quid sit. Si sine causa? quae fuerit causa, mox videro; interea hoc tenebo, si ob……")
     
     static let dummyAudioNote: Note = .init(id: 1, title: "winter vacation", date: "12 Dec 2017", noteType: .audioNote, audio: "", image: nil, description: description)
     
