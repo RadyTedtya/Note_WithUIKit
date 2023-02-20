4bf28620e683ca874291b0efaeb257b1a748ba97
Tedtya Rady <tedtyarady@gmail.com>
2023-02-20 12:22:46 +1100

    [Added]: - Note.swift
    [Action]
    - Add note class with dummy data for all type of notes

---
+++ Note_WithUIKit/Note.swift
@@ -0,0 +1,26 @@
+//
+//  Note.swift
+//  Note_WithUIKit
+//
+//  Created by Tedtya rady on 20/2/2023.
+//
+
+import Foundation
+
+struct Note: Identifiable {
+    var id: Int
+    var title: String!
+    var date: String!
+    var noteType: NoteType!
+    var audio: String?
+    var image: String?
+    var description: String?
+    
+    static let description = "Tum dicere exorsus est laborum et via procedat oratio quaerimus igitur, quid sit. Si sine causa? quae fuerit causa, mox videro; interea hoc tenebo, si ob……"
+    
+    static let dummyImageNote: Note = .init(id: 1, title: "summer vacation", date: "12 Jan 2021", noteType: .imageNote, audio: nil, image: "image", description: description)
+    
+    static let dummyAudioNote: Note = .init(id: 1, title: "winter vacation", date: "12 Dec 2017", noteType: .audioNote, audio: "", image: nil, description: description)
+    
+    static let dummyReminderNote: Note = .init(id: 1, title: "winter vacation", date: "12 Dec 2017", noteType: .reminderNote, audio: nil, image: nil, description: description)
+}
