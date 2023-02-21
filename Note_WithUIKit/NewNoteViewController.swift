8b985cb485f77fa5632bff48b1384c62aacefe0e
Tedtya Rady <tedtyarady@gmail.com>
2023-02-21 19:41:35 +1100

    [Action] - Add navigation item to new note screen

--- Note_WithUIKit/NewNoteViewController.swift
+++ Note_WithUIKit/NewNoteViewController.swift
@@ -11,19 +11,11 @@ class NewNoteViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        navigationItem.rightBarButtonItem = .init(image: UIImage(systemName: "checkmark"), style: .plain, target: self, action: #selector(saveNote))
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @objc func saveNote() {
+        print("Save note!!")
     }
-    */
 
 }
