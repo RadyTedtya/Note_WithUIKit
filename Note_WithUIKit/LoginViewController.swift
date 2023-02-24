01ea6f38fa24c63575f1476c62c726cb81322467
Tedtya Rady <tedtyarady@gmail.com>
2023-02-24 17:06:31 +1100

    Added 2 more screens

---
+++ Note_WithUIKit/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  Note_WithUIKit
+//
+//  Created by Tedtya rady on 24/2/2023.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
