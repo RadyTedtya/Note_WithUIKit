8108b59b64f0ef60753efd9f0a06940cfb23568e
Tedtya Rady <tedtyarady@gmail.com>
2023-02-26 20:19:52 +1100

    [ADDED]: - User.swift - LoginViewModel.swift

---
+++ Note_WithUIKit/LoginViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewModel.swift
+//  Note_WithUIKit
+//
+//  Created by Tedtya rady on 26/2/2023.
+//
+
+import Foundation
+import UIKit
+import FirebaseCore
+import FirebaseFirestore
+
+class LoginViewModel {
+    
+//    let user
+}
---
+++ Note_WithUIKit/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  Note_WithUIKit
+//
+//  Created by Tedtya rady on 26/2/2023.
+//
+
+import Foundation
+
+struct User {
+    let id: Int!
+    let username: String!
+    let password: String!
+    let email: String!
+}
