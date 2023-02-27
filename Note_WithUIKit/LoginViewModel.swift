435b781ef5ae150828612bad841e55c3c6626bd3
Tedtya Rady <tedtyarady@gmail.com>
2023-02-28 00:49:07 +1100

    [ACTION]: - updated LoginViewModel

--- Note_WithUIKit/LoginViewModel.swift
+++ Note_WithUIKit/LoginViewModel.swift
@@ -12,5 +12,32 @@ import FirebaseFirestore
 
 class LoginViewModel {
     
-//    let user
+    var db: Firestore!
+    
+    
+    func firebaseSetup() {
+        //[Start Firebase setup]
+        let setting = FirestoreSettings()
+        Firestore.firestore().settings = setting
+        //[End setup]
+        db = Firestore.firestore()
+    }
+
+    //Add new document with generated ID
+    func addNewDocument() {
+        var ref: DocumentReference? = nil
+        ref = db.collection("users").addDocument(data: [
+            "email":"ted@gmail.com",
+            "username":"ted",
+            "password":"12345"
+        ]) { error in
+            if let error = error {
+                print("Error adding document: \(error)")
+            } else {
+                print("Document added with ID: \(ref!.documentID)")
+            }
+            
+        }
+    }
+    
 }
