fa991d11b1e11d5056b3ab80abbaa04c27d2f484
Tedtya Rady <tedtyarady@gmail.com>
2023-02-28 00:56:55 +1100

    [MODIFIED]: - RegisterViewController.swift - RegisterViewController.xib
    [ACTION]:
    - Update registration screen

--- Note_WithUIKit/RegisterViewController.swift
+++ Note_WithUIKit/RegisterViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class RegisterViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    func setup() {
+        emailTextField.placeholder = "Email"
+        usernameTextField.placeholder = "Username"
+        passwordTextField.placeholder = "Password"
+        emailTextField.text = ""
+        usernameTextField.text = ""
+        passwordTextField.text = ""
+        
+    }
 
-
+    @IBAction func registerButton(_ sender: Any) {
+        
+    }
 }
