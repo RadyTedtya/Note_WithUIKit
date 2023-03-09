6ff80c218222424bbec4692250a4f8ba67dfe80c
tedtya rady <tedtyarady@email.com>
2023-03-09 16:33:46 +1100

    Call back function/closure login and update result with alert

--- Note_WithUIKit/LoginViewController.swift
+++ Note_WithUIKit/LoginViewController.swift
@@ -6,13 +6,17 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
     
     private let _viewModel: LoginViewModel! = .init()
-    
+    var isLoading: Bool = false
+    var alert = UIAlertController(title: "Title", message: nil, preferredStyle: .alert)
+
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,17 +38,26 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func loginButton(_ sender: Any) {
+        isLoading = true
         _viewModel.user.email = emailTextField!.text?.lowercased()
         _viewModel.user.password = passwordTextField!.text?.lowercased()
-        _viewModel.loginUser()
-        if NoteApp.shared.isLogin {
-            print("Login Success")
-            navigationController?.popViewController(animated: true)
-        } else {
-            navigationController?.pushViewController(LoginViewController(), animated: true)
-        }
+        _viewModel.loginUser(closure: {
+            if NoteApp.shared.isLogin {
+                self.alert.title = "Login Success"
+                let alertAction = UIAlertAction(title: "OK", style: .default) { (action:UIAlertAction!) in
+                    self.navigationController?.popViewController(animated: true)
+                }
+                self.alert.addAction(alertAction)
+                self.present(self.alert, animated: true, completion: nil)
+            } else {
+                self.alert.title = "Login Fail"
+                let alertAction = UIAlertAction(title: "OK", style: .default)
+                self.alert.addAction(alertAction)
+                self.present(self.alert, animated: true, completion: nil)
+            }
+        })
     }
-
+    
     @IBAction func registerButton(_ sender: Any) {
         navigationController?.pushViewController(RegisterViewController(), animated: true)
     }
